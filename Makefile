aa37811d468ee3b33a45c78f1ccbae00af1765e7
nachh <iortego42madrid@gmail.com>
2024-02-02 18:53:40 +0100

    Nueva estrucrtura del proyecto

---
+++ Makefile
@@ -0,0 +1,58 @@
+NAME= strings.a
+
+CC = gcc
+AR = ar
+ARFLAGS = -rc
+RM = rm -rf 
+OBJDIR = build
+SRCDIR = src
+INCDIR = include
+
+
+# CFLAGS = -Wall -Wextra -Werror  -I $(INCDIR) -fsanitize=address
+CFLAGS = -Wall -Wextra -Werror -I $(INCDIR)
+
+SRCS =	\
+		io.c \
+		cp.c \
+		split.c \
+		strings.c 
+
+
+
+OBJS := $(addprefix $(OBJDIR)/,$(SRCS:%.c=%.o))
+
+all: $(NAME)
+
+sanitize: CFLAGS += -fsanitize=address -g3 
+sanitize: $(OBJS) 
+	@echo "[strings]->>\033[34m [◊] SANITIZE MODE ON [◊]\033[0m"
+	$(AR) $(ARFLAGS) $(NAME) $^
+	
+debug: CFLAGS += -g3
+debug: $(OBJS)
+	@echo "[strings]->> \033[33m [∆] DEBUG MODE ON [∆]\033[0m"
+	$(AR) $(ARFLAGS) $(NAME) $^
+
+$(NAME): $(OBJS)
+	$(AR) $(ARFLAGS) $@ $^
+
+$(OBJDIR): 
+	mkdir $@
+
+$(OBJDIR)/%.o: $(SRCDIR)/%.c | $(OBJDIR)
+	$(CC) $(CFLAGS) -c $< -o $@ 
+
+clean:
+	$(RM) $(OBJDIR)
+
+fclean: clean
+	$(RM) $(NAME)
+
+re: fclean all
+
+red: fclean debug
+
+res: fclean sanitize
+
+.PHONY: sanitize debug res red re clean fclean all
--- include/strings.h
+++ include/strings.h
@@ -5,14 +5,7 @@
 # include <unistd.h>
 
 typedef struct s_str *t_string;
-// typedef enum {
-//     PRINT,
-//     PRINTE,
-//     PRINTFD,
-//     STRCPY,
-//     STRNCPY,
-//     FUNC_NUM
-// } t_func;
+
 void        dtor(t_string *this);
 void        ctor(t_string *this, char *data);
 void        print(t_string this);
@@ -27,9 +20,6 @@ t_string    split(t_string this, char delimitator);
 t_string    nsplit(t_string this, char delimitator);
 void        printlist(t_string this);
 void        clearlist(t_string this, size_t size);
-// void    getfunc_io(t_string  this);
-// void    getfunc_cp(t_string  this);
-
 struct s_str {
     size_t      start;
     size_t      end;
@@ -38,13 +28,5 @@ struct s_str {
     char        mask;
     t_string    *list;
     size_t      list_size;
-    // void        *(*f[FUNC_NUM])(t_string this, void *extra);
-    // void        (*print)(t_string   this);
-    // void        (*printe)(t_string   this);
-    // void        (*printfd)(t_string   this, int fd);
-    // t_string    (*strcpy)(t_string  this);
-    // t_string    (*strncpy)(t_string  this, int size);
 };
-
-
 #endif
