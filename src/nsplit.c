59ca5e652195a430cefc81388b0c5cd3f45ae2f0
nachh <iortego42madrid@gmail.com>
2024-02-25 18:05:11 +0100

    Función lstostr

--- src/nsplit.c
+++ src/nsplit.c
@@ -54,3 +54,32 @@ t_string	*nsplit(t_string this, size_t splits, size_t *position)
 	list[w - 1] = NULL;
 	return (dtor(&str), list);
 }
+
+t_string	lstostr(t_string *list)
+{
+	t_string	str;
+	size_t		length;
+	size_t		i;
+	size_t		n;
+
+	if (!list)
+		return (NULL);
+	n = 0;
+	length = 0;
+	ctor(&str, NULL);
+	if (!str)
+		return (str);
+	while (list[n])
+		length += len(list[n++]);
+	n = 0;
+	str->data = malloc(length + 1);
+	str->data_len = length;
+	while (list[n++])
+	{
+		i = list[n - 1]->start;
+		while (i < list[n - 1]->end)
+			str->data[str->end++] = list[n - 1]->data[i++];
+	}
+	str->data[length] = 0;
+	return (str);
+}
